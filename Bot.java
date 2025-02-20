4005b6f9b2a989baa7a74530f951b3d96bd63d7f
rbxo0128 <104380956+rbxo0128@users.noreply.github.com>
2025-02-20 17:09:46 +0900

    basic code

--- Bot.java
+++ Bot.java
@@ -3,28 +3,30 @@ import java.net.http.*;
 import java.time.*;
 import java.util.*;
 
-public class Bot{
-    public static void main(String[] args) {
+public class Bot {
+    public static void main(String[] args) { // 진입부분
+        // 이게 있어야 이 클래스를 실행했을 때 작동을 함
+        // 웹훅을 만들 거임 -> URL 필요함
+        // 환경변수로 받아올 것임 -> yml 파일에서 전달하게
         String webhookUrl = System.getenv("SLACK_WEBHOOK_URL");
-        
+
+        // Java 11 -> fetch
         HttpClient client = HttpClient.newHttpClient();
-
+        // 요청을 얹힐 거다
         HttpRequest request = HttpRequest.newBuilder()
-            .url(URI.create(webhookUrl))
+            .uri(URI.create(webhookUrl))
             .header("Content-Type", "application/json")
-            .POST(HttpRequest.BodyPublishers.ofString("{\"text\":\"테스트 메시지\"}"))
+            .POST(HttpRequest.BodyPublishers.ofString("{\"text\":\"테스트 메시지\"}")) 
             .build();
-
-        try{
+        
+        try {
             HttpResponse<String> response = client.send(
                 request, HttpResponse.BodyHandlers.ofString()
             );
-            System.out.println("요청 코드: "+response.statusCode());
-            System.out.println("응답 결과: "+response.body());
-
-        }   
-        catch(Exception e){
+            System.out.println("요청 코드: " + response.statusCode());
+            System.out.println("응답 결과: " + response.body());
+        } catch (Exception e) {
             e.printStackTrace();
-        }     
+        }
     }
 }
